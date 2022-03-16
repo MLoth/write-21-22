182371aeac0363320ebd972e986b36ff5aaa8d2c
Martijn Loth <MLoth@users.noreply.github.com>
2022-03-16 09:09:10 +0100

    feat: added fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import AppLoading from 'expo-app-loading'
+import {
+  useFonts,
+  PlayfairDisplay_400Regular,
+  PlayfairDisplay_700Bold,
+} from '@expo-google-fonts/playfair-display'
 
 export default function App() {
-  return (
-    <View>
-      <StatusBar style="auto" />
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  )
+  const [fontsLoaded, error] = useFonts({
+    PlayfairDisplay_400Regular,
+    PlayfairDisplay_700Bold,
+  })
+
+  // FONT INLADEN: fonts zijn overal nodig, pas nadat fonts geladen zijn, kan de app verder runnen!
+  if (!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <View>
+        <StatusBar style="auto" />
+
+        <Text
+          style={{
+            fontFamily: 'PlayfairDisplay_700Bold',
+            marginTop: 64,
+          }}
+        >
+          Open up App.tsx to start working on your app!
+        </Text>
+      </View>
+    )
+  }
 }
